8b24f5fd53901d82bf07826784f3139a8d75270d
Shreshth Khare <shreshth.khare.221102@gmail.com>
2022-08-25 18:48:03 +0530

    pointers and characters

--- ArrayPointers.cpp
+++ ArrayPointers.cpp
@@ -3,20 +3,7 @@ using namespace std;
 
 int main()
 {
-    int a[10];
-    cout<<a<<endl;
-    cout<<&a[0]<<endl;
+    int a[]={1,2,3};
 
-    a[0]=5;
-    cout<<*a<<endl;
-
-    a[1]=10;
-    cout<<*(a+1)<<endl;
-
-    int *p=&a[0];
-    cout<<a<<endl;
-    cout<<p<<endl;
-
-    cout<<&p<<endl;
-    cout<<&a<<endl;
+    cout<<a;
 }
